8e40ccd76704c64983346af512da44041e521572
Taku <takelow1994@me.com>
2016-09-13 12:43:13 +0900

    modified IO

--- lib/generators/hatena/install/install_generator.rb
+++ lib/generators/hatena/install/install_generator.rb
@@ -1,14 +1,12 @@
 require 'rails'
-require 'thor'
 
 module Hatena
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../../expand_sorcery")
       def create_initializer_file
-        # include Thor::Actions
-        create_file "#{Rails.root}/config/initializers/expand_sorcery/external_add_hatena.rb", './external-add-hatena.rb'
-        create_file "#{Rails.root}/config/initializers/expand_sorcery/providers/hatena.rb", './sorcery/providers/hatena.rb'
+        create_file "#{Rails.root}/config/initializers/expand_sorcery/external_add_hatena.rb", open('./external-add-hatena.rb')
+        create_file "#{Rails.root}/config/initializers/expand_sorcery/providers/hatena.rb", open('./sorcery/providers/hatena.rb')
       end
     end
   end
