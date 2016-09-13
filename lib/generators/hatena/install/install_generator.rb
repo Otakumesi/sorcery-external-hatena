97056ba2ba809cd7ee376c239e3c92fbc733f80b
Taku <takelow1994@me.com>
2016-09-13 20:56:35 +0900

    modified method

--- lib/generators/hatena/install/install_generator.rb
+++ lib/generators/hatena/install/install_generator.rb
@@ -6,7 +6,7 @@ module Hatena
     class InstallGenerator < Rails::Generators::Base
       #source_root File.expand_path(File.expand('../../../expand_sorcery', __FILE__))
       def create_initializer_file
-        source_path = File.expand_path(File.expand('../../../expand_sorcery', __FILE__))
+        source_path = File.expand_path(File.expand_('../../../expand_sorcery', __FILE__))
         FileUtils.cp_r "#{Rails.root}/config/initializers/expand_sorcery/external_add_hatena.rb", "#{source_path}/expand_sorcery/external_add_hatena.rb"
         FileUtils.cp_r "#{Rails.root}/config/initializers/expand_sorcery/sorcery/providers/hatena.rb", "#{source_root}/expand_sorcery/sorcery/providers/hatena.rb"
       end
--- sorcery-external-hatena.gemspec
+++ sorcery-external-hatena.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |s|
 
   s.add_dependency "rails", "~> 5.0.0"
   s.add_dependency "sorcery", "~>0.9"
+  s.add_development_dependency "pry"
 end
