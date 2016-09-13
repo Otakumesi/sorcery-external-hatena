50342a89304fa23ac15f424338d991c885ef58a9
Taku <takelow1994@me.com>
2016-09-13 12:47:44 +0900

    modified path

--- lib/generators/hatena/install/install_generator.rb
+++ lib/generators/hatena/install/install_generator.rb
@@ -5,8 +5,8 @@ module Hatena
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../../expand_sorcery")
       def create_initializer_file
-        create_file "#{Rails.root}/config/initializers/expand_sorcery/external_add_hatena.rb", open('./external-add-hatena.rb')
-        create_file "#{Rails.root}/config/initializers/expand_sorcery/providers/hatena.rb", open('./sorcery/providers/hatena.rb')
+        create_file "#{Rails.root}/config/initializers/expand_sorcery/external_add_hatena.rb", open('../../../expand_sorcery/external-add-hatena.rb')
+        create_file "#{Rails.root}/config/initializers/expand_sorcery/providers/hatena.rb", open('../../../expand_sorcery/sorcery/providers/hatena.rb')
       end
     end
   end
