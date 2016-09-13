9bd2dd7692492a1efbd567b992192fb21c9c5b02
Taku <takelow1994@me.com>
2016-09-13 21:00:43 +0900

    modified method_name

--- lib/generators/hatena/install/install_generator.rb
+++ lib/generators/hatena/install/install_generator.rb
@@ -4,9 +4,8 @@ require 'fileutils'
 module Hatena
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      #source_root File.expand_path(File.expand('../../../expand_sorcery', __FILE__))
       def create_initializer_file
-        source_path = File.expand_path(File.expand_('../../../expand_sorcery', __FILE__))
+        source_path = File.expand_path(File.expand_path('../../../expand_sorcery', __FILE__))
         FileUtils.cp_r "#{Rails.root}/config/initializers/expand_sorcery/external_add_hatena.rb", "#{source_path}/expand_sorcery/external_add_hatena.rb"
         FileUtils.cp_r "#{Rails.root}/config/initializers/expand_sorcery/sorcery/providers/hatena.rb", "#{source_root}/expand_sorcery/sorcery/providers/hatena.rb"
       end
